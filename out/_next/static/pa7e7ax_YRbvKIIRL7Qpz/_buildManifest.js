93cac5aa94b7d91555dc77379886dfbf0aabf886
jgoa156 <rockband5.0@hotmail.com>
2022-02-15 20:06:30 -0400

    Build v 1.0

---
+++ out/_next/static/pa7e7ax_YRbvKIIRL7Qpz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/efa19b0fddc8c08d.css","static/chunks/pages/index-066a1fd18a3ae54b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
