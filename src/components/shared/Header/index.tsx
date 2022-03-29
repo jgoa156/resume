d30f925ef21970a785f66ec1658025b5815f8fc0
jgoa156 <rockband5.0@hotmail.com>
2022-03-29 17:20:58 -0400

    Added load condition for header menu

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -21,6 +21,7 @@ export default function Header() {
 	const isTablet = useMediaQuery({
 		query: "(max-width: 1024px)"
 	});
+	const [loaded, setLoaded] = useState(false);
 	const [scrolled, setScrolled] = useState(false);
 	const [showGoToTop, setShowGoToTop] = useState(false);
 	const [showSidenav, setShowSidenav] = useState(false);
@@ -37,6 +38,7 @@ export default function Header() {
 	// Go to top
 	const bannerHeight = isTablet ? 400 : 500;
 	useEffect(() => {
+		setLoaded(true);
 		window.addEventListener("scroll", () => {
 			setScrolled(window.scrollY >= bannerHeight - 50);
 			setShowGoToTop(window.scrollY >= bannerHeight + 50);
@@ -56,21 +58,23 @@ export default function Header() {
 					Guilherme
 				</TitleWrapper>
 
-				{!isTablet
-					? (
-						<LinkWrapper>
-							<Scrollspy items={links.map((link) => link.href)} currentClassName={"spy"} offset={-100}>
-								{links.map((link, index) => {
-									return <a key={index} href={`#${link.href}`} data-to-scrollspy-id={link.href}>{link.title}</a>
-								})}
-							</Scrollspy>
-						</LinkWrapper>
-					)
-					: (
-						<Burger onClick={() => setShowSidenav(true)} aria-expanded={showSidenav}>
-							<i className={"fas fa-bars"} />
-						</Burger>
-					)
+				{loaded
+					? !isTablet
+						? (
+							<LinkWrapper>
+								<Scrollspy items={links.map((link) => link.href)} currentClassName={"spy"} offset={-100}>
+									{links.map((link, index) => {
+										return <a key={index} href={`#${link.href}`} data-to-scrollspy-id={link.href}>{link.title}</a>
+									})}
+								</Scrollspy>
+							</LinkWrapper>
+						)
+						: (
+							<Burger onClick={() => setShowSidenav(true)} aria-expanded={showSidenav}>
+								<i className={"fas fa-bars"} />
+							</Burger>
+						)
+					: null
 				}
 
 				<GoToTop href={"#banner"} showGoToTop={showGoToTop} className={"fas fa-arrow-up"} />
